76cd6cffa5ec353f0cc6af4bcc0499e41849f4f5
Enoch Abiodun <g00380316@atu.ie>
2023-12-05 20:07:25 +0000

    completely removed old server

--- my-faithpal/src/app/(pages)/(userPages)/bible/page.js
+++ my-faithpal/src/app/(pages)/(userPages)/bible/page.js
@@ -1,6 +1,23 @@
+"use client"
 
+import React from "react";
+import Dropdown from "@/components/bible/dropdown";
+
+const options = [
+    { value: 'Genesis 1', label: 'Genesis' },
+    { value: 'Exodus 1', label: 'Exodus' },
+    { value: 'Leviticus 1', label: 'Leviticus' },
+];
+
+const handleOptionSelect = (selectedOption) => {
+        console.log(`Selected option: ${selectedOption}`);
+}
+    
 export default function bible() {
     return (
-        <h1>Bible page</h1>
+    <div>
+    <h1>Dropdown Example</h1>
+    <Dropdown options={options} onSelect={handleOptionSelect} />
+    </div>
     )
 }
---
+++ my-faithpal/src/components/bible/dropdown.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Dropdown = ({ options, onSelect }) => {
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleSelect = (option) => {
+    setSelectedOption(option);
+    onSelect(option);
+};
+
+return (
+    <div>
+        <label>Select an bible:</label>
+        <select
+        value={selectedOption}
+        onChange={(e) => handleSelect(e.target.value)}
+        >
+        <option value="" disabled>Select an option</option>
+        {options.map((option) => (
+        <option key={option.value} value={option.value}>
+            {option.label}
+        </option>
+        ))}
+    </select>
+    </div>
+    );
+};
+
+export default Dropdown;
