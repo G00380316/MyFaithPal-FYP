886276cd5fb81df13f6e72aec69854c12197f523
Enoch Abiodun <g00380316@atu.ie>
2024-02-08 06:13:39 +0000

    Can create chatroom through frontend

--- my-faithpal/src/components/chat/potentialChats.js
+++ my-faithpal/src/components/chat/potentialChats.js
@@ -1,9 +1,11 @@
 import { ChatContext } from "@/context/chatContext";
 import { useContext } from "react";
 import styles from "@/components/chat/chat.module.css";
+import { useSession } from "next-auth/react";
 
 export default function PotentialChats () {
-    const { potentialChats } = useContext(ChatContext);
+    const { potentialChats, createChat } = useContext(ChatContext);
+    const { data: session } = useSession();
 
     console.log("PotentialChats: ", potentialChats);
 
@@ -12,7 +14,7 @@ export default function PotentialChats () {
             <div className={styles.all_users}>
                 {potentialChats && potentialChats?.map((u, index) => {
                     return (
-                        <div className={styles.single_user} key={index}>
+                        <div className={styles.single_user} key={index} onClick={()=> createChat(session?.user?._id, u._id)}>
                         {u.name}
                             <span className={styles.user_online}></span>
                         </div>
