57c36973c48cc4b3467481f27b35dedf57bbe13c
Enoch Abiodun <g00380316@atu.ie>
2024-02-09 14:24:22 +0000

    added time to messages and got user and recipient messages loading on different sides of chatbox

--- my-faithpal/src/components/chat/chatBox.js
+++ my-faithpal/src/components/chat/chatBox.js
@@ -2,14 +2,18 @@ import { ChatContext } from '@/context/chatContext';
 import { useFetchRecipientUser } from '@/hooks/useChatboxFetchRecipient';
 import React, { useContext } from 'react'
 import styles from "@/components/chat/chat.module.css";
+import moment from 'moment';
+import { useSession } from 'next-auth/react';
 
 export default function chatBox() {
 
+  const { data: session } = useSession();
   const { currentChat, messages, isMessagesLoading } = useContext(ChatContext);
   const { recipientUser }  = useFetchRecipientUser(currentChat);
 
   console.log( "This is chatBox current Chat: ",currentChat)
-  console.log( "This is chatBox recipient User: ",recipientUser)
+  console.log("This is chatBox recipient User: ", recipientUser)
+  console.log("These are messages:", messages)
 
   if (!recipientUser)
     return (
@@ -33,8 +37,9 @@ export default function chatBox() {
       <div className={styles.chat_messages}>
       <div className={styles.messages_box}>
         <div className={styles.messages}>
-        {messages && messages.map((messages, index) => <div  key={index} className={styles.message}>
-                <span>{messages.text}</span>
+        {messages && messages.map((message, index) => <div  key={index} className={message?.user === session?.user?._id ? styles.message : styles.user_message}>
+          <span>{message.text}</span>
+          <span className={styles.date}>{moment(message.createdAt).calendar()}</span>
               </div>
             )}
           </div>
