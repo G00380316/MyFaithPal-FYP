f5230af55040775a013503ea7097d3904d8f6340
Enoch Abiodun <g00380316@atu.ie>
2024-02-10 04:43:52 +0000

    User is now able to send messages through the chatbox

--- my-faithpal/src/components/chat/chatBox.js
+++ my-faithpal/src/components/chat/chatBox.js
@@ -1,19 +1,22 @@
 import { ChatContext } from '@/context/chatContext';
 import { useFetchRecipientUser } from '@/hooks/useChatboxFetchRecipient';
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styles from "@/components/chat/chat.module.css";
 import moment from 'moment';
 import { useSession } from 'next-auth/react';
+import InputEmojiWithRef from 'react-input-emoji';
 
 export default function chatBox() {
 
   const { data: session } = useSession();
-  const { currentChat, messages, isMessagesLoading } = useContext(ChatContext);
-  const { recipientUser }  = useFetchRecipientUser(currentChat);
+  const { currentChat, messages, isMessagesLoading , sendTextMessage} = useContext(ChatContext);
+  const { recipientUser } = useFetchRecipientUser(currentChat);
+  const [textMessage, setTextMessage] = useState("");
 
   console.log( "This is chatBox current Chat: ",currentChat)
   console.log("This is chatBox recipient User: ", recipientUser)
   console.log("These are messages:", messages)
+  console.log("Message input: ", textMessage)
 
   if (!recipientUser)
     return (
@@ -44,6 +47,10 @@ export default function chatBox() {
             )}
           </div>
         </div>
+      </div>
+      <div className={styles.chat_input}>
+        <InputEmojiWithRef value={textMessage} onChange={setTextMessage} fontFamily="nunito" borderColor="rgba(72,112,223,0.2)" />
+        <button className={styles.send_button} onClick={() => sendTextMessage(textMessage , currentChat._id , setTextMessage)}>Send</button>
       </div>
     </div >
   );
