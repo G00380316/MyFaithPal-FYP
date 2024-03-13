b542a8621a3bfb414441d83764ce70609dc9b667
Enoch Abiodun <g00380316@atu.ie>
2024-03-13 01:13:46 +0000

    Updated Webscraping code to build the json file not overwrite with the use of concatenation

--- my-faithpal_ai_server/routes/faithgpt.js
+++ my-faithpal_ai_server/routes/faithgpt.js
@@ -34,16 +34,29 @@ router.post('/webscrape', async (req, res) => {
         listItems.each((idx, el) => {
             QA.question = $(el).children("h1").children("*[itemprop = 'name headline']").text();
             QA.answer = $(el).children("*[itemprop = 'articleBody']").text();
-            questions.push(questions);
         });
 
+        questions.push(QA);
+        
         const checkData = await sourceKnowledge.create({ question: QA.question, answer: QA.answer });
 
         console.dir(checkData);
 
-        // Write questions array to question.json file and also returning Json and Message when data has been stored in DataBase
-        await fs.promises.writeFile("question.json", JSON.stringify(questions, null, 2));
+        let existingQuestions = [];
+        try {
+            const fileData = await fs.promises.readFile("question.json", "utf-8");
+            existingQuestions = JSON.parse(fileData);
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                console.error("Error reading existing data from file:", err);
+            }
+        }
+
+        const mergedQuestions = existingQuestions.concat(questions);
+
+        await fs.promises.writeFile("question.json", JSON.stringify(mergedQuestions, null, 2));
         console.log("Successfully written data to file");
+
         res.status(201).json({ response: checkData, message: "Scraping completed successfully!" });
         
     } catch (err) {
