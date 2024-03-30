a0d0edd8827f206ce43a76d4aee8ec8e3e476f80
Enoch Abiodun <g00380316@atu.ie>
2024-03-30 21:45:36 +0000

    added bash script that automatically runs all my servers

---
+++ rundev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start servers
+npm start --prefix my-faithpal_server &
+npm start --prefix my-faithpal_server/socket &
+npm start --prefix my-faithpal_ai_server &
+npm start --prefix my-faithpal_ai_server/socket &
+
+# Wait for servers to start
+sleep 5 # Adjust this if necessary
+
+# Run Faithpal
+npm run dev --prefix my-faithpal
