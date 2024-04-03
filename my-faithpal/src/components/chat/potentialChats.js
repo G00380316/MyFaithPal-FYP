8d0c846366bc999c43f785b36c9a875ee154f83d
Enoch Abiodun <g00380316@atu.ie>
2024-04-03 05:25:58 +0100

    Got search in the chatroom working to create and open previous chats

--- my-faithpal/src/components/chat/potentialChats.js
+++ my-faithpal/src/components/chat/potentialChats.js
@@ -1,18 +1,75 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { useContext } from 'react';
 import styles from '@/components/chat/chat.module.css';
 import { useSession } from 'next-auth/react';
 import { Box, Input } from '@mui/joy';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import { ChatContext } from '@/context/chatContext';
+import { getRequest } from '@/util/service';
+import { Avatar } from '@mui/joy';
 
 export default function PotentialChats() {
-    const { potentialChats, createChat, onlineUsers } = useContext(ChatContext);
+    const { potentialChats, createChat, onlineUsers, updateCurrentChat, userChats } = useContext(ChatContext);
     const { data: session } = useSession();
     const [searchQuery, setSearchQuery] = useState('');
+    const [allUsers, setAllUsers] = useState([]);
+    const [inputFocused, setIsInputFocused] = useState(false);
 
-    // Filter potential chats based on search query
-    const filteredChats = potentialChats.filter(chat => chat.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    /*
+    useEffect(() => {
+        const getUsers = async () => {
+            const response = await getRequest(`/api/getAllUsers`);
+
+            if (response.error) {
+                console.log("Error getting Users", response);
+                return;
+            }
+
+            const users = response?.user?.filter(u => u._id !== session?.user?._id);
+            setAllUsers(users);
+        };
+
+        getUsers();
+    }, [session]);*/
+
+    const fetchAllUsers = async () => {
+        const response = await getRequest(`/api/getAllUsers`);
+
+        if (response.error) {
+            console.log("Error getting Users", response);
+            return;
+        }
+
+        const users = response?.user?.filter(u => u._id !== session?.user?._id);
+        setAllUsers(users);
+    };
+
+    useEffect(() => {
+
+        if (inputFocused) {
+            fetchAllUsers();
+        }
+
+    }, [inputFocused]);
+
+    const filteredChats = allUsers.filter(user => user.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
+    const handleChatAction = (userId) => {
+
+        const existingChat = userChats?.find(chat =>
+            (chat.participants?.includes(session?.user?._id) && chat.participants.includes(userId))
+        );
+
+        if (existingChat) {
+
+            updateCurrentChat(existingChat);
+
+        } else {
+        
+            createChat(session?.user?._id, userId);
+        
+        }
+    };
 
     return (
         <div className={styles.all_users}>
@@ -23,24 +80,27 @@ export default function PotentialChats() {
                     placeholder="Search"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
+                    onFocus={() => setIsInputFocused(true)}
+                    onBlur={() => setIsInputFocused(false)}
                 />
             </Box>
-            {filteredChats.map((u, index) => {
-                const isUserOnline = onlineUsers?.some(user => user.userID === u._id);
-                if (u.name === 'AI') {
-                    return null;
-                }
-                return (
-                    <div
-                        className={styles.single_user}
-                        key={index}
-                        onClick={() => createChat(session?.user?._id, u._id)}
-                    >
-                        {u.name}
-                        <span className={isUserOnline ? styles.user_online : ''}></span>
+                {inputFocused || searchQuery.trim() !== ''  ?
+                filteredChats.map((user, index) => (
+                    user.name !== 'AI' && (
+                        <div className={styles.single_user} key={index} onClick={() => handleChatAction(user._id)}>
+                            <Avatar src={user?.image || ""} sx={{ borderColor: 'background.body', height:24, width:24, mr:1 }}/>
+                            {user.name}
+                            <span className={onlineUsers?.some(onlineUser => onlineUser.userID === user._id) ? styles.user_online : ''}></span>
+                        </div>
+                    )))
+                :
+                potentialChats.map((user, index) => (
+                    user.name !== 'AI' && (
+                        <div className={styles.single_user} key={index} onClick={() => handleChatAction(user._id)}>
+                            <Avatar src={user?.image || ""} sx={{ borderColor: 'background.body', height:24, width:24, mr:1 }}/>
+                            {user.name}
+                            <span className={onlineUsers?.some(onlineUser => onlineUser.userID === user._id) ? styles.user_online : ''}></span>
+                        </div>)))}
                     </div>
                 );
-            })}
-        </div>
-    );
-}
+            }
